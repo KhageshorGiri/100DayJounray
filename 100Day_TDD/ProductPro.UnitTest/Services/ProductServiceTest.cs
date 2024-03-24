1980e72db2be027aeb4ae25286e4ee800c6dd566
KhageshorGiri <girikhageshor2432@gmail.com>
2024-03-24 13:10:34 +0545

    Unit test for Services

---
+++ 100Day_TDD/ProductPro.UnitTest/Services/ProductServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Moq;
+using ProductPro.Domain.IRepositories;
+using ProductPro.Domain.Models;
+using ProjectPro.Application.Services;
+
+namespace ProductPro.Application.Test.Services
+{
+    public class ProductServiceTest
+    {
+        private readonly Random rand = new Random();
+        private readonly Mock<IProductRepository> repositoryStub = new();
+
+        [Fact]
+        public async Task GetAllAsync_WithExistingProductList_ReturnProductList()
+        {
+            // Arrange
+            var expectedList = new[] { CreateRandomProduct(), CreateRandomProduct(), CreateRandomProduct() };
+
+            repositoryStub.Setup(repo => repo.GetAllAsync())
+                .ReturnsAsync(expectedList);
+
+            var service = new ProductService(repositoryStub.Object);
+
+            // Act
+            var result = await service.GetAllAsync();
+
+            // Asserts
+            Assert.NotNull(result);
+            result.Should().BeEquivalentTo(expectedList,
+                option => option.ComparingByMembers<Product>());
+        }
+
+
+
+
+        private Product CreateRandomProduct()
+        {
+            return new()
+            {
+                Id = rand.Next(100),
+                ProductName = "Test Prduct",
+                ProductDescription = "Test Description",
+                Price = rand.Next(1000)
+            };
+        }
+    }
+}
