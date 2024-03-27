cee9d46592a07b7d7bde41d00a223df37b5fb4d9
KhageshorGiri <girikhageshor2432@gmail.com>
2024-03-27 21:39:20 +0545

    Configuring serilog | learning logging

--- 100Day_TDD/ProductPro.API/Program.cs
+++ 100Day_TDD/ProductPro.API/Program.cs
@@ -4,37 +4,62 @@ using ProductProd.Infrastructure.Data;
 using ProductProd.Infrastructure.Repositories;
 using ProjectPro.Application.IServices;
 using ProjectPro.Application.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// db registration
-builder.Services.AddDbContext<ProductProDbContext>(option => 
-        option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));    
-
-// service regrestration
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Adding serlog configuration
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
+
+    Log.Information("Starting web application");
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // db registration
+    builder.Services.AddDbContext<ProductProDbContext>(option =>
+            option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
+
+    // service regrestration
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+    builder.Services.AddScoped<IProductService, ProductService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
