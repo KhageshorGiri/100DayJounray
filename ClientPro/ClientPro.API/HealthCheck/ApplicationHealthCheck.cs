80b7e564e42dcc6d4db4d51ed2a548243abf6c77
KhageshorGiri <girikhageshor2432@gmail.com>
2024-03-30 13:31:06 +0545

    API health check: APi and sql server connection

---
+++ ClientPro/ClientPro.API/HealthCheck/ApplicationHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ClientPro.API.HealthCheck
+{
+    public class ApplicationHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                // Perform your application health checks here
+
+                // For example, check if your application's critical components are functioning correctly.
+
+                // If everything is fine, return Healthy status
+                return Task.FromResult(HealthCheckResult.Healthy("Application is healthy."));
+            }
+            catch (Exception ex)
+            {
+                // If an exception occurs during the health check, return Unhealthy status
+                return Task.FromResult(HealthCheckResult.Unhealthy("An error occurred while checking application health.", ex));
+            }
+        }
+    }
+}
--- ClientPro/ClientPro.API/Program.cs
+++ ClientPro/ClientPro.API/Program.cs
@@ -1,10 +1,15 @@
+using ClientPro.API.HealthCheck;
 using ClientPro.Application.Interfaces;
 using ClientPro.Application.Services;
 using ClientPro.Domain.IRepositories;
 using ClientPro.Infrastructure.DataContext;
 using ClientPro.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using System.Net.Mime;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +37,11 @@ try
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
+    //health checks
+    builder.Services.AddHealthChecks()
+        //.AddCheck<ApplicationHealthCheck>("application_health_check")
+        .AddSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection"));
+
     // service configuration
     builder.Services.AddScoped<IClientRepository, ClientRepository>();
     builder.Services.AddScoped<IClientService, ClientService>();
@@ -50,6 +60,30 @@ try
     app.UseAuthorization();
 
     app.MapControllers();
+
+    // Health check Middleware configuration
+    app.MapHealthChecks("/health",new HealthCheckOptions()
+    {
+        //Predicate = (_) => false,
+        ResponseWriter = async(context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(entry => new
+                    {
+                        name = entry.Key.ToString(),
+                        status = entry.Value.Status.ToString(),
+                        exception = entry.Value.Exception is not null ? entry.Value.Exception.Message : "none",
+                        duration = entry.Value.Duration.ToString()
+                    })
+                });
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
 
     app.Run();
 }
