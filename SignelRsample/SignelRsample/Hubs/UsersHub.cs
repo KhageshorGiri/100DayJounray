9fc1b15d8d100810aab45464dae5e287480b5cd5
KhageshorGiri <girikhageshor2432@gmail.com>
2024-05-07 23:04:50 +0545

    First porject for signalr

---
+++ SignelRsample/SignelRsample/Hubs/UsersHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignelRsample.Hubs
+{
+    public class UsersHub : Hub
+    {
+        public static int TotalViews = 0;
+        public static int TotalUserCount = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUserCount++;
+            Clients.All.SendAsync("UpdateTotalUserCount", TotalUserCount).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUserCount--;
+            Clients.All.SendAsync("UpdateTotalUserCount", TotalUserCount).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+
+            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        }
+    }
+}
---
+++ SignelRsample/SignelRsample/wwwroot/js/UserCount.js
@@ -0,0 +1,38 @@
+﻿
+// Create a connection
+var userCountConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/userCount")
+    .build();
+                                        
+// Connect to method that hub invokes aks recivenotificactions form hubs
+userCountConnection.on("UpdateTotalViews", (value) => {
+    var newCountSpan = document.getElementById('totalViewCounter');
+    newCountSpan.innerText = value.toString();
+});
+
+userCountConnection.on("UpdateTotalUserCount", (value) => {
+    var newUserCountSpan = document.getElementById('totalUserCounter');
+    newUserCountSpan.innerText = value.toString();
+});
+
+// invoke hub method aka send notification to hub
+function newWindowLoadedOnClient() {
+    userCountConnection.send("NewWindowLoaded");
+}
+
+
+// start connection
+userCountConnection.start()
+    .then(afterFulfilled, afterRejected);
+
+
+// function to handle fulfilled condition
+function afterFulfilled() {
+    console.log("Connection to user hub sucessfull.");
+    newWindowLoadedOnClient();
+}
+
+// function to handle rejected condition
+function afterRejected() {
+
+}
