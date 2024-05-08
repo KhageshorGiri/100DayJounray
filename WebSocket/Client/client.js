a1a0d7c4154b239c45391a530f521517696141fe
KhageshorGiri <girikhageshor2432@gmail.com>
2024-05-08 21:39:31 +0545

    setting up client side project template

---
+++ WebSocket/Client/client.js
@@ -0,0 +1,20 @@
+var connID = document.getElementById("connIDLabel");
+var stateLabel = document.getElementById("stateLabel");
+
+var connectionUrl = document.getElementById("connectionUrl");
+var connectButton = document.getElementById("connectButton");
+var closeButton = document.getElementById("closeButton");
+
+var sendMessage = document.getElementById("sendMessage");
+var sendButton = document.getElementById("sendButton");
+
+var commsLog = document.getElementById("commsLog");
+var recipients = document.getElementById("recipients");
+
+// setting up connection url
+
+connectionUrl.value = "ws://localhost:5000";
+
+connectButton.onclick = () => {
+    
+}
