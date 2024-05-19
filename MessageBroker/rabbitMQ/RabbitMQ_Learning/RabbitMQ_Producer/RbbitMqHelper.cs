c683585ef67fb92ab2d6518de1898aed2f40df4f
girikhageshor2432@gmail.com <girikhageshor2432@gmail.com>
2024-05-19 18:38:07 +0545

    single producer | Multiple consumer

--- MessageBroker/rabbitMQ/RabbitMQ_Learning/RabbitMQ_Producer/RbbitMqHelper.cs
+++ MessageBroker/rabbitMQ/RabbitMQ_Learning/RabbitMQ_Producer/RbbitMqHelper.cs
@@ -20,17 +20,51 @@ namespace RabbitMQ_Producer
             };
             var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            channel.QueueDeclare("SinglePublisherConsumer_queue", 
+            channel.QueueDeclare("SinglePublisherConsumer_queue",
                 durable: true,
                 exclusive: false,
                 autoDelete: false,
-                arguments: null );
+                arguments: null);
 
             var message = new { Name = "Message Producer", Message = "hello form producer" };
             var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
 
             // publish message
-            channel.BasicPublish(exchange:"", routingKey: "", basicProperties: null, body: body);
+            channel.BasicPublish(exchange: "", routingKey: "", basicProperties: null, body: body);
+        }
+    }
+
+    // One producer produce message to mutiple consumer
+    public static class QueueProducer
+    {
+        public static void Publish()
+        {
+            var factory = new ConnectionFactory
+            {
+                // rabbitMq server url with usernaeme and password
+                Uri = new Uri("amqp://guest:guest@localhost:5772")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("SingleProducerMultipleConsumner",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            int count = 0;
+            while (count <= 10)
+            {
+                var message = new { Name = "Message Producer", Message = "hello form producer" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                // publish message
+                channel.BasicPublish(exchange: "", routingKey: "", basicProperties: null, body: body);
+
+                count++;
+                Thread.Sleep(1000);
+            }
+
         }
     }
 }
--- MessageBroker/rabbitMQ/RabbitMQ_Learning/RabbitMQ_consumer/RabbitMqHelper.cs
+++ MessageBroker/rabbitMQ/RabbitMQ_Learning/RabbitMQ_consumer/RabbitMqHelper.cs
@@ -38,4 +38,36 @@ namespace RabbitMQ_consumer
 
         }
     }
+
+
+    // One producer produce message to mutiple consumer
+    public static class QueueConsumer
+    {
+        public static void Consume()
+        {
+            var factory = new ConnectionFactory
+            {
+                // rabbitMq server url with usernaeme and password
+                Uri = new Uri("amqp://guest:guest@localhost:5772")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("SingleProducerMultipleConsumner",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            // Consume message
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) => {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume(queue: "SingleProducerMultipleConsumner", autoAck: true, consumer: consumer);
+
+        }
+    }
 }
