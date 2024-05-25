1c43eee6a3e0d54beb08169d92b021b84a3dc988
girikhageshor2432@gmail.com <girikhageshor2432@gmail.com>
2024-05-25 17:45:19 +0545

    Adding api for polly response service

---
+++ polly/PollyDemo/ResponseService/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResponseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("[action]/{id:int}")]
+        public async Task<IActionResult> GetResponse(int id)
+        {
+            var ranodm = new Random();
+
+            if(ranodm.Next(1, 100) >= id)
+                return BadRequest(StatusCodes.Status400BadRequest);
+
+            return Ok();
+        }
+
+
+    }
+}
